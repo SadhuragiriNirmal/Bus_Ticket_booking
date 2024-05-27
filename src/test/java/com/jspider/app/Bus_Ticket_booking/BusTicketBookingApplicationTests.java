418d61d532dde1f915b7b99e8667d853aa707415
nirmal <sadhuragirinirmal@gmail.com>
2024-05-27 14:34:57 +0530

    project initiated

---
+++ src/test/java/com/jspider/app/Bus_Ticket_booking/BusTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jspider.app.Bus_Ticket_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
